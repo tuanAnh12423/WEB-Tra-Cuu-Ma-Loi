8a91d6c23dc0c21b1105f9b9b542f78b36d9236b
tuanAnh12423 <ptanh01662629504@gmail.com>
2026-07-29 11:08:50 +0700

    Cap Nhat

--- src/data/errorFridge.ts
+++ src/data/errorFridge.ts
@@ -4,13 +4,14 @@ export const fridgeErrors: ErrorItem[] = [
   {
     id: "F01",
     category: "fridge",
-    code: "F1",
+    code: "(R001) Cả tủ không lạnh",
     title: "Tủ lạnh không làm lạnh",
-    description: "Máy nén vẫn chạy nhưng ngăn mát không có hơi lạnh.",
+    description: "Mở tủ ra không thấy có hơi lạnh",
     steps: [
-      { text: "Kiểm tra nhiệt độ cài đặt (Nên để ngăn mát 3-5°C, ngăn đá -18°C)." },
-      { text: "Kiểm tra gioăng cao su cửa tủ xem có bị hở không." },
-      { text: "Kiểm tra lỗ thông gió ngăn đá xuống ngăn mát có bị cản trở không." }
+      { text: " Kiểm tra tủ có nguồn không? Vui lòng kiểm tra đèn led có sáng và máy nén có hoạt động không, xem lại ổ cắm điện" },
+      { text: "KH cách cài đặt theo từng model đã đúng cách chưa?" },
+      { text: "Nếu Led sáng + 2 bên hông tủ có nóng không +  cài đặt đúng" },
+      { text: "Báo ASP kiểm tra" }
     ],
     images: ["https://lh3.googleusercontent.com/d/17RWtS1AsR0Qfxtd3B_Te09xQX3AagB9v",],
   }
