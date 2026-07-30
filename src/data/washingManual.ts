d2c27727505b4324470315d3a9a8e8f3ff414cb1
tuanAnh12423 <ptanh01662629504@gmail.com>
2026-07-30 19:35:31 +0700

    Cap Nhat

--- src/data/washingManual.ts
+++ src/data/washingManual.ts
@@ -58,7 +58,7 @@ export const washingManuals: ManualItem[] = [
     pdfUrl: "https://drive.google.com/file/d/1Z_Y6jX3MqydxFX87rl2Ktgip1OyNQu12/preview"
     },
     {
-    id: "mw7",
+    id: "mw8",
     category: "washing",
     brand: "TOSHIBA",
     model: "AW-DK1000FV, AW-M1000FV, AW-M1100PV, AW-DM1100PV, AW-M1100JV, AW-DM1100JV",
@@ -66,7 +66,7 @@ export const washingManuals: ManualItem[] = [
     pdfUrl: "https://drive.google.com/file/d/1H7Exh8FQd9dYgkTputWVGSHfF7Rmhi-T/preview"
     },
     {
-    id: "mw8",
+    id: "mw9",
     category: "washing",
     brand: "TOSHIBA",
     model: "TW-T21BU130",
@@ -74,7 +74,7 @@ export const washingManuals: ManualItem[] = [
     pdfUrl: "https://drive.google.com/file/d/1uOLk4UF2Qmh8ZarChxKTQfNZCki35mvy/preview"
     },
     {
-    id: "mw9",
+    id: "mw10",
     category: "washing",
     brand: "TOSHIBA",
     model: "TWD-T35BP160",
@@ -82,7 +82,7 @@ export const washingManuals: ManualItem[] = [
     pdfUrl: "https://drive.google.com/file/d/174gVTy6-Xb3bT1wXsfE8bKyWYLOI5w7k/preview"
     },
     {
-    id: "mw10",
+    id: "mw11",
     category: "washing",
     brand: "TOSHIBA",
     model: "TWD-T27",
@@ -90,11 +90,99 @@ export const washingManuals: ManualItem[] = [
     pdfUrl: "https://drive.google.com/file/d/1rHvnqKpDAZr4nxShul_gpGELZC4eiJOc/preview"
     },
     {
-    id: "mw11",
+    id: "mw12",
     category: "washing",
     brand: "TOSHIBA",
     model: "TW-T37BZ",
     title: "Sách HDSD Máy giặt TOSHIBA TW-T37BZ",
     pdfUrl: "https://drive.google.com/file/d/1xGfZX7z0InJF-MAfXDypqmwhUN6GXTae/preview"
-    },
+  },
+    {
+    id: "mw13",
+    category: "washing",
+    brand: "TOSHIBA",
+    model: "TD-T40BS100HWV(KK)",
+    title: "Sách HDSD Bộ máy giặt, máy sấy TOSHIBA TD-T40BS100HWV(KK)",
+    pdfUrl: "https://drive.google.com/file/d/1qE87Wk_aBYD6JVygWHR2Z2Of_y3cMuYE/preview"
+  },
+  {
+    id: "mw14",
+    category: "washing",
+    brand: "TOSHIBA",
+    model: "TW-T40BZP140MWV(KK)",
+    title: "Sách HDSD máy giặt TOSHIBA TW-T40BZP140MWV(KK)",
+    pdfUrl: "https://drive.google.com/file/d/1pr0q5-22AuRJRTffAUAWu7zpDpEtSEiZ/preview"
+  },
+  {
+    id: "mw15",
+    category: "washing",
+    brand: "TOSHIBA",
+    model: "TW-T33B130UWV(MK)",
+    title: "Sách HDSD máy giặt TOSHIBA TW-T33B130UWV(MK)",
+    pdfUrl: "https://drive.google.com/file/d/133URODTIcPkfe4adDZlvVAS0Vr8L46jT/preview"
+  },
+  {
+    id: "mw16",
+    category: "washing",
+    brand: "TOSHIBA",
+    model: "AW-T06D1500LV(MK)",
+    title: "Sách HDSD máy giặt TOSHIBA AW-T06D1500LV(MK)",
+    pdfUrl: "https://drive.google.com/file/d/1rQ_gTgrTpuhByEq6LXQyc9uKgTbfaQUQ/preview"
+  },
+  {
+    id: "mw17",
+    category: "washing",
+    brand: "TOSHIBA",
+    model: "AW-DK1300KV(SG), AW-DM1400",
+    title: "Sách HDSD máy giặt TOSHIBA AW-DK1300KV(SG), AW-DM1400",
+    pdfUrl: "https://drive.google.com/file/d/1I3vbIuAMhQHwPzaS7qUHNJ3m6r0i0Zf8/preview"
+  },
+  {
+    id: "mw18",
+    category: "washing",
+    brand: "TOSHIBA",
+    model: "AW-DM1600LV(SG)",
+    title: "Sách HDSD máy giặt TOSHIBA AW-DM1600LV(SG)",
+    pdfUrl: "https://drive.google.com/file/d/1EwNPzMt11SKBXijSZicoGLoLTzD19eJW/preview"
+  },
+  {
+    id: "mw19",
+    category: "washing",
+    brand: "TOSHIBA",
+    model: "TW-T25BZP140MWV(MG)",
+    title: "Sách HDSD máy giặt TOSHIBA TW-T25BZP140MWV(MG)",
+    pdfUrl: "https://drive.google.com/file/d/1ktEr2SCkmRaEJgefpZCGBRhDp9rIxXst/preview"
+  },
+  {
+    id: "mw20",
+    category: "washing",
+    brand: "TOSHIBA",
+    model: "TD-T21B90HWV(MG)",
+    title: "Sách HDSD máy sấy TOSHIBA TD-T21B90HWV(MG)",
+    pdfUrl: "https://drive.google.com/file/d/1OOJeZc7vimGVyxXAt2pY_DgmCtdeoMZX/preview"
+  },
+  {
+    id: "mw21",
+    category: "washing",
+    brand: "TOSHIBA",
+    model: "TD-T37BS90HWV(WT)",
+    title: "Sách HDSD máy sấy TOSHIBA TD-T37BS90HWV(WT)",
+    pdfUrl: "https://drive.google.com/file/d/1awETtQ1LIXdRmAD-pmm8j-4a-GbbdjFW/preview"
+  },
+  {
+    id: "mw22",
+    category: "washing",
+    brand: "TOSHIBA",
+    model: "TD-BP110GHV(MG)",
+    title: "Sách HDSD máy sấy TOSHIBA TD-BP110GHV(MG)",
+    pdfUrl: "https://drive.google.com/file/d/1DCLfOkRMnXm3jxcgADBC4kI-e34xs0wC/preview"
+  },
+  {
+    id: "mw23",
+    category: "washing",
+    brand: "TOSHIBA",
+    model: "TD-T25BS110HWV(MG)",
+    title: "Sách HDSD máy sấy TOSHIBA TD-T25BS110HWV(MG)",
+    pdfUrl: "https://drive.google.com/file/d/1Tx06yP75sbFcFWlRn8gpAEghjNeV-x-z/preview"
+  },
 ];
